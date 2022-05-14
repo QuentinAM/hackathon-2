ddd82e03ffa11cd79c0de43d67153c0d81534637
FoxT3c <quentin.abel-marceau@epita.fr>
2022-05-14 03:20:29 +0200

    Rat

---
+++ src/components/footer.svelte
@@ -0,0 +1,26 @@
+<script>
+    const routes = [
+        {
+            name: 'About',
+            route: '/about'
+        }
+    ]
+</script>
+
+<footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2022
+        <a href="/" class="hover:underline">
+            RAT.
+        </a>All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        {#each routes as route}
+            <li>
+                <a href={route.route} class="mr-4 hover:underline md:mr-6 ">
+                    {route.name}
+                </a>
+            </li>
+        {/each}
+    </ul>
+</footer>
