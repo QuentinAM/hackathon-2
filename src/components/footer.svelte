55d5aef03113e5e4cd0fb9c0f044efeeabab23cf
FoxT3c <quentin.abel-marceau@epita.fr>
2022-05-15 13:07:50 +0200

    Added websitecarbon

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -6,5 +6,11 @@
                 RAT.
             </a>All Rights Reserved.
         </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <p class="mr-4 md:mr-6 ">This web page is cleaner than
+                    97 % of web pages tested by <a class="hover:underline" href="https://www.websitecarbon.com/website/hackthon2-onrender-com/">websitecarbon</a></p>
+            </li>
+        </ul>
     </footer>
 </div>
